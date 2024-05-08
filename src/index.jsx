1ba1229e2c7f60283e1b06961f72203f078419fc
ekorahy <ekorahy@gmail.com>
2024-05-08 13:41:05 +0700

    build(deps): add prop-types, react-router-dom and tailwind

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
 import { createRoot } from 'react-dom/client';
 
-const helloMindSpace = <h1>Hello, MindSpace!</h1>;
+import './styles/index.css'
+
+const helloMindSpace = <h1 className='font-bold text-indigo-400'>Hello, MindSpace!</h1>;
 
 const root = createRoot(document.getElementById('root'));
 root.render(helloMindSpace);
