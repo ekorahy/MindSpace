e7c981d8088540c870ec3bff52e5fb00cd3d52d9
ekorahy <ekorahy@gmail.com>
2024-05-08 13:31:05 +0700

    chore: initial commit

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import { createRoot } from 'react-dom/client';
+
+const helloMindSpace = <h1>Hello, MindSpace!</h1>;
+
+const root = createRoot(document.getElementById('root'));
+root.render(helloMindSpace);
