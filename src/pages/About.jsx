bf7b1ee547020146782b67df2567a14066c32e97
ekorahy <ekorahy@gmail.com>
2024-05-13 16:14:28 +0700

    feat(non-authedUser): add about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,15 @@
 export const About = () => {
   return (
-    <div>About Page</div>
-  )
-}
+    <div>
+      <h2 className='font-bold text-xl mb-4'>MindSpace</h2>
+      <img
+        className='rounded w-full mx-auto mb-2'
+        src='/public/logo.jpg'
+        alt=''
+      />
+      <p>
+        is a free, easy-to-use, and secure app for managing personal data notes.
+      </p>
+    </div>
+  );
+};
